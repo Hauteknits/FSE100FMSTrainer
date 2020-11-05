5f82e90aa56deb2b683e559e4cf0714f9cfee16e
Hauteknits <holdenclarke@yahoo.com>
2020-11-05 12:19:10 -0700

    Web Server Commit
    +Added a web server, allowing the program to be fully functional by itself

---
+++ XAMPP/cgi-bin/cgi.cgi
@@ -0,0 +1,5 @@
+#!"C:\Users\holde\OneDrive\School Stuff\FSE100\FSE100FMSTrainer\FSE100FMSTrainer\XAMPP\perl\bin\perl.exe"
+
+print "Content-Type: text/html\n\n";
+
+print "OK";
---
+++ dev_start.bat
@@ -0,0 +1,9 @@
+@echo off
+
+REM Creating a Newline variable (the two blank lines are required!)
+set NLM=^
+set NL=^^^%NLM%%NLM%^%NLM%%NLM%
+
+echo Open your web browser of choice and navigate to "127.0.0.1"%NLM%
+echo When you are finished with the program, close this window
+XAMPP\apache\bin\httpd.exe
